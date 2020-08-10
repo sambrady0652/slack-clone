787c6cf1fefb853c8b0f998e6cc3f26a36f537b9
Sam Brady <sambrady0652@gmail.com>
2020-08-10 05:43:39 -0500

    heroku test

--- app.js
+++ app.js
@@ -50,8 +50,8 @@ wss.on('connection', (ws) => {
 //REACT BUILD CONFIG FOR HEROKU
 //TODO FIX THIS!!!!!
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('/', (req, res) => {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
